b81828ab00c80f03da737aeba277ca5d79201c99
Gerardo Rafael Cabrera Herrera <gecabrera@outlook.com>
2024-11-23 13:13:59 -0600

    Implementar modelo y base de datos al programa

--- app/Livewire/Game.php
+++ app/Livewire/Game.php
@@ -4,16 +4,22 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use Illuminate\Support\Collection;
+use App\Models\Player;
+use Illuminate\Support\Str;
 
 class Game extends Component
 {
     public $players;
+
+    private string $gameUuid;
 
     public function mount(): void 
     {
         $this->players = Collection::times(5, fn () => [
             'name' => ''
         ]);
+
+        $this->gameUuid = Str::uuid();
     }
 
     public function render()
@@ -31,5 +37,26 @@ class Game extends Component
     public function removePlayer($key): void
     {
         $this->players->forget($key);
+    }
+
+    public function startGame() 
+    {
+        $validated = $this->validate([
+            'players.*.name' => 'required|string|max:100',
+        ], messages: [
+            'players.*.name.required' => 'El campo :attribute es obligatorio.',
+            'players.*.name.string' => 'El campo :attribute debe ser una cadena de texto.',
+            'players.*.name.max' => 'El campo :attribute no puede tener más de :max caracteres.',
+        ], attributes: [
+            'players.*.name' => 'nombre del jugador',
+        ]);
+
+        $players = collect($validated['players'])->map(function ($player) {
+            return Player::create([
+                'name' => $player['name'],
+                'game_uuid' => $this->gameUuid,
+                'uuid' => Str::uuid(),
+            ]);
+        });
     }
 }
---
+++ app/Models/Player.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Player extends Model
+{
+    protected $guarded = [];
+
+    protected function casts(): array
+    {
+        return [
+            'saw_secret_friend' => 'boolean'
+        ];
+    }
+
+    public function secretFriend(): HasOne 
+    {
+        return $this->hasOne(self::class, 'secret_friend_id');
+    }
+}
