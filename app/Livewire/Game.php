b70cdc59ed7cf5509bb9c5777d04d4977114bb99
Gerardo Rafael Cabrera Herrera <gecabrera@outlook.com>
2024-11-23 12:32:56 -0600

    Agregar funcionalidades de la pagina

--- app/Livewire/Game.php
+++ app/Livewire/Game.php
@@ -3,11 +3,33 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Collection;
 
 class Game extends Component
 {
+    public $players;
+
+    public function mount(): void 
+    {
+        $this->players = Collection::times(5, fn () => [
+            'name' => ''
+        ]);
+    }
+
     public function render()
     {
         return view('livewire.game');
+    }
+
+    public function addPlayer(): void 
+    {
+        $this->players->push([
+            'name' => ''
+        ]);
+    }
+
+    public function removePlayer($key): void
+    {
+        $this->players->forget($key);
     }
 }
--- resources/views/livewire/game.blade.php
+++ resources/views/livewire/game.blade.php
@@ -6,16 +6,20 @@
             </svg>
         </div>
         <form class="p-12 pt-20">
-            <div class="mb-5">
-                <div class="flex items-center gap-4 text-lg">
-                    <svg class="absolute ml-3" width="24" viewBox="0 0 24 24">
-                        <path d="M20.822 18.096c-3.439-.794-6.64-1.49-5.09-4.418 4.72-8.912 1.251-13.678-3.732-13.678-5.082 0-8.464 4.949-3.732 13.678 1.597 2.945-1.725 3.641-5.09 4.418-3.073.71-3.188 2.236-3.178 4.904l.004 1h23.99l.004-.969c.012-2.688-.092-4.222-3.176-4.935z" />
-                    </svg>
-                    <input type="text" placeholder="Nombre del jugador" class="w-full py-2 pl-12 bg-gray-200 md:py-4 focus:outline-none">
-                    <button type="button">X</button>
+            @foreach ($players as $key => $player)
+                <div class="mb-5">
+                    <div class="flex items-center gap-4 text-lg">
+                        <svg class="absolute ml-3" width="24" viewBox="0 0 24 24">
+                            <path
+                                d="M20.822 18.096c-3.439-.794-6.64-1.49-5.09-4.418 4.72-8.912 1.251-13.678-3.732-13.678-5.082 0-8.464 4.949-3.732 13.678 1.597 2.945-1.725 3.641-5.09 4.418-3.073.71-3.188 2.236-3.178 4.904l.004 1h23.99l.004-.969c.012-2.688-.092-4.222-3.176-4.935z" />
+                        </svg>
+                        <input type="text" placeholder="Nombre del jugador"
+                            class="w-full py-2 pl-12 bg-gray-200 md:py-4 focus:outline-none">
+                        <button type="button" wire:click="removePlayer({{$key}})">X</button>
+                    </div>
                 </div>
-            </div>
-            <button type="button" class="w-full p-2 font-medium text-gray-900 uppercase bg-gradient-to-b from-gray-300 to-gray-400 md:p-4 hover:from-gray-400 hover:to-gray-500">
+            @endforeach
+            <button type="button" class="w-full p-2 font-medium text-gray-900 uppercase bg-gradient-to-b from-gray-300 to-gray-400 md:p-4 hover:from-gray-400 hover:to-gray-500" wire:click="addPlayer">
                 Añadir Jugador
             </button>
             <button type="button" class="w-full p-2 mt-2 font-medium text-white uppercase bg-gradient-to-b from-gray-700 to-gray-800 md:p-4 hover:from-gray-800 hover:to-gray-900">
